dda91db85e6d1be3d23c74923fae54e22428d266
Karan Goel <karan@goel.im>
2013-07-04 12:10:14 +0530

    Mechanize not working, Selenium works but is slow

--- pyquora.py
+++ pyquora.py
@@ -1,11 +1,28 @@
 # Trying to login to quora right now
 
 import mechanize
+import cookielib
 
 br = mechanize.Browser() # create a browser object
-br.set_handle_robots(False) #ignore robots
-br.set_handle_refresh(False) # can hang without this
-br.set_headers = [('User-Agent', 'PyQuora')]
+br.set_handle_equiv(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+br.set_headers = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)')]
 
-response = br.open('http://www.quora.com')
-print response.read()
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+res = br.open('http://www.quora.com')
+
+print res.info()
+
+br.select_form(nr = 0)
+
+print br.form, '\n\n'
+
+br.form['email'] = ''
+br.form['password'] = ''
+res = br.submit()
+
+print res.read()
